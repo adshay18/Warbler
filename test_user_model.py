d5e5981a38aa21ca5eb27ca8eed2a7e9a31678ed
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-02-18 15:58:55 -0500

    add tests for User model

--- test_user_model.py
+++ test_user_model.py
@@ -56,3 +56,85 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+        
+    def test_is_following(self):
+        """Detection of when one user is following another user"""
+        
+        user1 = User(
+            email="test1@test.com",
+            username="testuser1",
+            password="HASHED_PASSWORD1"
+        )
+        db.session.add(user1)
+        db.session.commit()
+        
+        user2 = User(
+            email="test2@test.com",
+            username="testuser2",
+            password="HASHED_PASSWORD2"
+        )
+        db.session.add(user2)
+        db.session.commit()
+        
+        # Test no following relationship between user1 and user2
+        self.assertEqual(user1.is_following(user2), False)
+        
+        # user1 starts following user2
+        test_follow = Follows(user_being_followed_id=user2.id, user_following_id=user1.id)
+        db.session.add(test_follow)
+        db.session.commit()
+        
+        self.assertEqual(user1.is_following(user2), True)
+        
+        
+    def test_is_followed_by(self):
+        """Detection of users followers"""
+        user1 = User(
+            email="test1@test.com",
+            username="testuser1",
+            password="HASHED_PASSWORD1"
+        )
+        db.session.add(user1)
+        db.session.commit()
+        
+        user2 = User(
+            email="test2@test.com",
+            username="testuser2",
+            password="HASHED_PASSWORD2"
+        )
+        db.session.add(user2)
+        db.session.commit()
+        
+        # test that user2 is not following user1
+        self.assertEqual(user1.is_followed_by(user2), False)
+        
+        # user2 followed user1 back, test that user
+        follow_back = Follows(user_being_followed_id=user1.id, user_following_id=user2.id)
+        db.session.add(follow_back)
+        db.session.commit()
+        self.assertEqual(user1.is_followed_by(user2), True)
+        
+    def test_signup(self):
+        '''Test signup classmethod'''
+        
+        user = User.signup(username='test', password='testpassword', email="test@email.com", image_url='test.url.png')
+        db.session.commit()
+        #Test that user was created
+        self.assertEqual(user.username, 'test')
+        self.assertEqual(f'{User.query.get(user.id)}', f'<User #{user.id}: test, test@email.com>')
+        
+        #Test that user isn't created if .signup is missing positional arguments
+        self.assertRaises(TypeError, User.signup, username='test1')
+        
+    def test_authenticate(self):
+        '''Test user authentication'''
+        
+        user1 = User.signup(username='testuser1', password="HASHED_PASSWORD1", email='test@test.gmail.com', image_url='test_img.png')
+        db.session.commit()
+        
+        # Returns correct user, if found
+        self.assertEqual(User.authenticate(username='testuser1', password="HASHED_PASSWORD1"), User.query.get(user1.id))
+        
+        # Does not return user when not found
+        self.assertNotEqual(User.authenticate(username='WRONG_USERNAME', password="HASHED_PASSWORD1"), User.query.get(user1.id))
+        self.assertNotEqual(User.authenticate(username='testuser1', password="WRONG_PASSWORD"), User.query.get(user1.id))
